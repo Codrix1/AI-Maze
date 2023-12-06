1fb9383467bb2c4067f99041e95bb5d27974db3e
Codrix <Workcodrix@gmail.com>
2023-12-06 23:01:01 +0200

    git commit -main 'grid.py'

--- grid.py
+++ grid.py
@@ -1,2 +1,14 @@
 import pygame
-from pygame.locals import *
+
+pygame.init()
+color = (255,255,255) 
+position = (0,0) 
+canvas = pygame.display.set_mode((500,500)) 
+
+while not exit: 
+    canvas.fill(color) 
+  
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            exit = True
+    pygame.display.update()
